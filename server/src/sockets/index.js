0d7f0eb664e9f82d274168de2b84155ef0b3920d
Sunnysaini2003 <sunnysaini.ldh@gmail.com>
2026-02-07 17:31:20 +0530

    Implement real-time chat with MongoDB persistence

--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -19,3 +19,29 @@ module.exports = (io) => {
   });
 
 };
+git add .
+const Chat = require('../modules/chat/chat.model');
+
+module.exports = (io) => {
+
+  io.on('connection', (socket) => {
+
+    socket.on('join_project', (projectId) => {
+      socket.join(`project_${projectId}`);
+    });
+
+    socket.on('send_message', async (data) => {
+
+      // save to MongoDB
+      await Chat.create({
+        projectId: data.projectId,
+        sender: data.sender,
+        message: data.message
+      });
+
+      io.to(`project_${data.projectId}`).emit('receive_message', data);
+    });
+
+  });
+
+};
