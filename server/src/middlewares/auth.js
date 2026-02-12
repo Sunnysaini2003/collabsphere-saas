0683cc65fd43c90ae712a8a5b95ff569bc217bd0
Sunnysaini2003 <sunnysaini.ldh@gmail.com>
2026-02-12 18:23:42 +0530

    middleware auth token issue fix

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,15 +1,18 @@
 // authorizing tokens
 const jwt = require('jsonwebtoken');
-
 module.exports = (req, res, next) => {
   const header = req.headers.authorization;
   if (!header) return res.status(401).json({ message: 'No token' });
 
+  // Extract token from "Bearer <token>"
+  const token = header.split(" ")[1]; 
+
   try {
+    // Use the variable you just extracted!
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch {
+  } catch (err) {
     res.status(401).json({ message: 'Invalid token' });
   }
 };
